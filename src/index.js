5808db8258fb262f132447ac8bb6a17346cdc5ec
xochitlmegalopolis <53918032+xochitlmegalopolis@users.noreply.github.com>
2020-08-20 18:15:23 -0700

    commit-5

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import {HashRouter} from "react-router-dom"
 import App from "./App";
 
 ReactDOM.render(
-  <HashRouter basename='/'><App /></BrowserRouter>,
+  <HashRouter basename='/'><App /></HashRouter>,
   document.getElementById('root')
 );
